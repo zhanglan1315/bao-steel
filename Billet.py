1e995187c839bb8b033d602b0cb1c3c11fb29706
zhanglan <zhanglan1315@gmail.com>
2019-10-17 10:21:35 +0800

    refact: 重新定义 Billet 属性

--- Billet.py
+++ Billet.py
@@ -1,37 +1,40 @@
 # steel.py
+
 class Billet:
-	# dataSource
+	# basic data
+	upid: ''
 	thick: ''
 	width: ''
+	steelSpec: ''
+
+	# process data
 	discharge: ''
-	rm_time_1: ''
-	fm_time_1: ''
-	steelspec: ''
-	staying_time_pre: ''
-	staying_time_1: ''
-	staying_time_2: ''
-	staying_time_soak: ''
-	in_fce_time: ''
-	upid: ''
-	da_1: ''
-	wait_t_1: ''
+	rmTime: ''
+	fmTime: ''
+	preStayingTime: ''
+	firstStayingTime: ''
+	secondStayingTime: ''
+	soakStayingTime: ''
+	inFceTime: ''
+	coolTime: ''
+	waitTime: ''
 
 	def __init__ (self, row):
 		self.setFromPandasCsvRow(row)
-		return
 
 	def setFromPandasCsvRow (self, row):
+		self.upid = row['upid']
 		self.thick = round(row['tgtplatethickness1'], 4)
 		self.width = round(row['tgtwidth'], 3)
-		self.discharge = round(row['discharge'], 3)
-		self.rm_time_1 = round(row['rm_time'], 3)
-		self.fm_time_1 = round(row['fm_time'], 3)
 		self.steelspec = row['steelspec']
-		self.staying_time_pre = row['staying_time_pre']
-		self.staying_time_1 = row['staying_time_1']
-		self.staying_time_2 = row['staying_time_2']
-		self.staying_time_soak = row['staying_time_soak']
-		self.in_fce_time = row['in_fce_time']
-		self.upid = row['upid']
-		self.da_1 = round(row['cooltime'], 3)
-		self.wait_t_1 = round(row['discharge_rm-fm'], 3)
+
+		self.discharge = round(row['discharge'], 3)
+		self.rmTime = round(row['rm_time'], 3)
+		self.fmTime = round(row['fm_time'], 3)
+		self.preStayingTime = row['staying_time_pre']
+		self.firstStayingTime = row['staying_time_1']
+		self.secondStayingTime = row['staying_time_2']
+		self.soakStayingTime = row['staying_time_soak']
+		self.inFceTime = row['in_fce_time']
+		self.coolTime = round(row['cooltime'], 3)
+		self.waitTime = round(row['discharge_rm-fm'], 3)
